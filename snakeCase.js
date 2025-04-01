afefea16dc34434a530668ec58e678e44ace7a47
rahul4019 <rahulkashyap4019@gmail.com>
2025-04-01 20:46:35 +0530

    Snake case conversion without using regex

--- snakeCase.js
+++ snakeCase.js
@@ -6,8 +6,20 @@ const input = {
   isEmployed: true,
 };
 
+// const convertToSnakeCase = (str) => {
+//   return str.replace(/([A-Z])/g, '_$1').toLowerCase();
+// };
+
 const convertToSnakeCase = (str) => {
-  return str.replace(/([A-Z])/g, '_$1').toLowerCase();
+  let result = '';
+  for (let char of str) {
+    if (char >= 'A' && char <= 'Z') {
+      result += '_' + char.toLowerCase();
+    } else {
+      result +=  char;
+    }
+  }
+  return result;
 };
 
 const snakeCaseObj = Object.entries(input).reduce((acc, curr) => {
