9fc44742cfd8387632298f79e28739e1b2444ccd
rahul4019 <rahulkashyap4019@gmail.com>
2025-04-08 20:11:00 +0530

    First repeating element in an array

---
+++ firstRepeatingElement.ts
@@ -0,0 +1,17 @@
+function firstRepeatingElement(nums: number[]): number {
+  const countMap: Map<number, number> = new Map();
+
+  for (const num of nums) {
+    countMap.set(num, (countMap.get(num) || 0) + 1);
+  }
+
+  for (const num of nums) {
+    if ((countMap.get(num) ?? 0) > 1) {
+      return num;
+    }
+  }
+  return -1;
+}
+
+console.log(firstRepeatingElement([10, 5, 3, 4, 3, 5, 6]));
+console.log(firstRepeatingElement([1, 2, 3, 4]));
